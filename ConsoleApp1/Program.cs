8d406e9805e179e5a47862ba210a6fcfacc905a3
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:03:10 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите x: ");
+        double x = double.Parse(Console.ReadLine());
+        Console.Write("Введите y: ");
+        double y = double.Parse(Console.ReadLine());
+
+        Solution solution = new(x, y);
+
+        if (y >= 0 && y <= 1) Console.WriteLine(solution.GetTopSolution());
+        else if (y <= 0 && y >= -1) Console.WriteLine(solution.GetBottomSolution());
+        else Console.WriteLine(solution.GetZeroSolution());
+    }
+}
---
+++ ConsoleApp1/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Solution
+    {
+        private double x;
+        private double y;
+
+        public Solution(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public string GetTopSolution()
+        {
+            if (x >= 0 && x <= 1 && y >= 0 + Math.Abs(x) && y <= 1 && y >= 0) return "Попадает";
+            else return "Не попадает";
+        }
+        public string GetBottomSolution()
+        {
+            if (x >= 0 && x <= 1 && y >= 0 - Math.Abs(x) && y <= 0 && y >= -1) return "Попадает";
+            else return "Не попадает";
+        }
+        public string GetZeroSolution()
+        {
+            if (x == 0 && y == 0) return "Попадает";
+            else return "Не попадает";
+        }
+    }
+}
